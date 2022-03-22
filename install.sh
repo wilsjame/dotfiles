115584caa1c0834f9ad5dd2cc71848b0f3b19585
James Wilson <jgwilson1214@gmail.com>
2022-03-22 01:23:33 -0700

    add .gitconfig

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# exit if a command returns non-zero (prevent snowballing)
+set -e
+
 echo "vvv"
 
 # run this script relative to its location e.g. ~/dotfiles/
 BASEDIR=$(dirname $0)
 cd $BASEDIR
+
+# machine specific stuff
+if uname | grep Darwin; then
+  echo running OS X
+elif uname --kernel-release | grep WSL2; then
+  # use git for windows credential manager
+  git config --file ./.gitconfig credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe"
+elif uname | grep Linux; then
+  echo running Linux
+fi
 
 # backup any existing dotfiles
 PREV_DOTFILES="./bk/dotfiles_backup_$(date -u +"%Y%m%d%H%M%S")"
@@ -24,9 +37,11 @@ function backup_if_exists() {
 # clean up common conflicts
 backup_if_exists ~/.tmux.conf
 backup_if_exists ~/.vimrc
+backup_if_exists ~/.gitconfig
 
 # link new dotfiles
-ln -s ${PWD}/.tmux.conf ~/.tmux.conf
-ln -s ${PWD}/.vimrc ~/.vimrc
+ln -sv ${PWD}/.tmux.conf ~/.tmux.conf
+ln -sv ${PWD}/.vimrc ~/.vimrc
+ln -sv ${PWD}/.gitconfig ~/.gitconfig
 
 echo "^^^"
